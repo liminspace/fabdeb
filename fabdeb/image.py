4a1162125c16173b3134f54126c00bc8f65bb019
liminspace <igormeln@gmail.com>
2016-12-06 20:21:40 +0200

    Update installing pngquant

--- fabdeb/image.py
+++ fabdeb/image.py
@@ -26,14 +26,12 @@ def install_pngquant_jpegtran():
     print_green('INFO: Install pngquant and jpegtran...')
     apt_install('libpng-dev liblcms2-dev libjpeg-progs', noconfirm=True)
     with cd('/tmp'):
-        sudo('wget -q https://github.com/pornel/pngquant/archive/master.zip')
-        sudo('unzip master.zip')
-        sudo('rm master.zip')
-    with cd('/tmp/pngquant-master'):
+        sudo('git clone --recursive https://github.com/pornel/pngquant.git')
+    with cd('/tmp/pngquant'):
         sudo('./configure --with-lcms2 && make')
         sudo('make install')
         if confirm('Do you want make symlink /usr/local/bin/pngquant to /bin/pngquant?'):
             sudo('ln -s /usr/local/bin/pngquant /bin/pngquant')
     with cd('/tmp'):
-        sudo('rm -rf pngquant-master')
+        sudo('rm -rf pngquant')
     print_green('INFO: Install pngquant and jpegtran...  OK')
